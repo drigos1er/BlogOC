4ade9ad23ea764f39d39be4a725456f12ff8699a
drigos1er <drigos1er@yahoo.fr>
2018-10-14 19:09:00 +0200

    ADD FILES LOGIN

---
+++ controller/controller.php
@@ -0,0 +1,12 @@
+<?php
+
+function home()
+{
+    require('http://localhost/BLOGOCR/BlogOC/view/frontend/home.html.twig');
+}
+
+
+function login()
+{
+    require('http://localhost/BLOGOCR/BlogOC/view/frontend/login.html.twig');
+}
---
+++ model/model.php
@@ -0,0 +1,14 @@
+<?php
+function getLogin()
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+
+}
