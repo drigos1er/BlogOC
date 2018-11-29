1ac6f1dcaa25fe6696f56dcff7713e33c7d7c5ce
drigos1er <drigos1er@yahoo.fr>
2018-11-29 18:17:21 +0100

    GESTION LOGIN IN HOME PAGE V2

---
+++ config/Config.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class Config
+{
+
+
+    const  DB_NAME='blogocbd';
+    const  DB_USER='root';
+    const  DB_PASS='';
+    const  DB_HOST='localhost';
+
+
+
+    private static $connectdb;
+
+    public static function getCdb(){
+
+   if(self::$connectdb===null){
+
+
+   self::$connectdb= new \Blog\core\Connexion(self::DB_NAME,self::DB_USER,self::DB_PASS,self::DB_HOST);
+
+   }
+
+   return self::$connectdb;
+
+
+    }
+
+}
